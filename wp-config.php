f8002e7ee78e6773e7f22d875a189eaf2a420bab
ljsherlock <lewis@ljsherlock.com>
2024-05-03 18:26:15 +0700

    update database config

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'ljsherlock-vinestovino' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'ljsherlock-vinestovino' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '7TQaOetMmyWJafjdCq2Z' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
